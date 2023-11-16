c9c8cd36dc398e906b40ddf5cf793674f7fd4b2c
Mona Mohsen Ahmed <129124836+Manonaahmd@users.noreply.github.com>
2023-11-16 22:23:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
-
+use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\backend\ExampleControllercarController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\backend\CarController;
+use App\Http\Controllers\backend\AddcarController;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
  Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
 
 //Route::get('test',function(){
@@ -33,56 +37,86 @@ use Illuminate\Support\Facades\Route;
 //     return $msg;
 // })->where('name',['MONA','ahm']);
 
-Route::prefix('Web structure')->group(function(){
-    Route::get('/', function(){
-    return 'About page';
-   });
+// Route::prefix('Web structure')->group(function(){
+//     Route::get('/', function(){
+//     return 'About page';
+//    });
 
-   Route::prefix('Web structure')->group(function(){
-    Route::get('/', function(){
-    return 'Contact us page';
-   });
+//    Route::prefix('Web structure')->group(function(){
+//     Route::get('/', function(){
+//     return 'Contact us page';
+//    });
 
-   Route::prefix('Web structure')->group(function(){
-    Route::get('/', function(){
-    return 'Support page';
-  });
+//    Route::prefix('Web structure')->group(function(){
+//     Route::get('/', function(){
+//     return 'Support page';
+//   });
 
-Route::get('call',function(){
-     return 'call page';
-    });
+// Route::get('call',function(){
+//      return 'call page';
+//     });
     
-Route::get('Ticket',function(){
-     return 'Ticket page';
+// Route::get('Ticket',function(){
+//      return 'Ticket page';
 
-Route::get('chat',function(){
-        return 'chat page';
+// Route::get('chat',function(){
+//         return 'chat page';
     
-    });
+//     });
     
- });
+//  });
     
-});     
+// });     
+
+// });
+
+// });
+// Route::prefix('Training')->group(function(){
+//     Route::get('/', function(){
+    //return 'HR page';
+   //});
+
+//Route::get('ICT',function(){
+     //return 'ICT page';
+    //});
+    
+//Route::get('Marketing',function(){
+    //return 'Marketingr page';
+
+//Route::get('Logistics',function(){
+        // return 'Logistics page';
+//});
+
+//});
+
+//});
+
+//Route::fallback(function(){
+  //  return Redirect('/');
+//});
+
+Route::get('cv',function(){
+    return view('cv');
 
 });
 
-});
-Route::prefix('Training')->group(function(){
-    Route::get('/', function(){
-    return 'HR page';
-   });
-
-Route::get('ICT',function(){
-     return 'ICT page';
-    });
-    
-Route::get('Marketing',function(){
-    return 'Marketingr page';
-
-Route::get('Logistics',function(){
-        return 'Logistics page';
-});
+Route::get('login',function(){
+    return view('login');
 
 });
 
+Route::get('addcar',function(){
+    return view('addcar');
+
 });
+
+Route::post('receive',function(){
+    return 'Data received';
+})->name('receive');
+
+
+Route::post('addcar1',function(){
+    return 'Datacar1';
+})->name('addcar1');
+
+Route::get('test1',[ExampleController::class,'test1']);
