bd19e0a02e859b375009b0157e17ae24ce8cb761
Mona Mohsen Ahmed <129124836+Manonaahmd@users.noreply.github.com>
2023-11-26 00:21:44 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\backend\ExampleControllercarController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\backend\CarController;
-use App\Http\Controllers\backend\AddcarController;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\NewwsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,106 +17,71 @@ use App\Http\Controllers\Controller;
 |
 */
 
- Route::get('/', function () {
-    return view ('welcome');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-//Route::get('test',function(){
-    //return 'Welcome to my first route';
-//});
+Route::get('test', function(){
+    return 'Welcome to my first route';
+});
 
-//Route::get('user/{name}/{age?}',function($name, $age=0){
-    //return 'the username is: ' . $name . ' and age is '. $age;
-//});
+Route::get ('user/{name}/{age?}',function($name,$age=0){
+    $msg = 'the username is : ' . $name;
+    if($age > 0){
+        $msg .= ' and the age is: '. $age ;
+    }
+    return $msg;
+})->whereIn('name',['Peter','Tony']);
 
-// //Route::get ('user/{name}/{age?}',function($name,$age=0){
-//     $msg = 'the username is : ' . $name;
-//     if($age > 0){
-//         $msg .= ' and age is: ' . $age ;
-//     }
-//     return $msg;
-// })->where('name',['MONA','ahm']);
+Route::prefix('product')->group(function(){
 
-// Route::prefix('Web structure')->group(function(){
-//     Route::get('/', function(){
-//     return 'About page';
-//    });
+    Route::get('/',function(){
+        return 'Products home page';
+    });
 
-//    Route::prefix('Web structure')->group(function(){
-//     Route::get('/', function(){
-//     return 'Contact us page';
-//    });
+    Route::get('laptop',function(){
+        return 'Laptop page';
+    });
 
-//    Route::prefix('Web structure')->group(function(){
-//     Route::get('/', function(){
-//     return 'Support page';
-//   });
+    Route::get('camera',function(){
+        return 'Camera page';
+    });
 
-// Route::get('call',function(){
-//      return 'call page';
-//     });
-    
-// Route::get('Ticket',function(){
-//      return 'Ticket page';
+    Route::get('projector',function(){
+        return 'Projectors page';
+    });
+});
 
-// Route::get('chat',function(){
-//         return 'chat page';
-    
-//     });
-    
-//  });
-    
-// });     
-
+// Route::fallback(function(){
+//     return redirect('/');
 // });
-
-// });
-// Route::prefix('Training')->group(function(){
-//     Route::get('/', function(){
-    //return 'HR page';
-   //});
-
-//Route::get('ICT',function(){
-     //return 'ICT page';
-    //});
-    
-//Route::get('Marketing',function(){
-    //return 'Marketingr page';
-
-//Route::get('Logistics',function(){
-        // return 'Logistics page';
-//});
-
-//});
-
-//});
-
-//Route::fallback(function(){
-  //  return Redirect('/');
-//});
 
 Route::get('cv',function(){
     return view('cv');
-
 });
 
-Route::get('login',function(){
-    return view('login');
+// Route::get('login',function(){
+//     return view('login');
+// });
 
-});
+// Route::post('receive',function(){
+//     return 'Data received';
+// })->name('receive');
 
-Route::get('addcar',function(){
-    return view('addcar');
+Route::get('login',[ExampleController::class, 'login']);
 
-});
+Route::post('receive',[ExampleController::class, 'received'])->name('receive');
 
-Route::post('receive',function(){
-    return 'Data received';
-})->name('receive');
+Route::get('test1',[ExampleController::class, 'test1']);
 
+Route::post('storeCar',[CarController::class, 'store'])->name('storeCar');
 
-Route::post('addcar1',function(){
-    return 'Datacar1';
-})->name('addcar1');
+Route::get('addCar',[CarController::class, 'create']);
 
-Route::get('test1',[ExampleController::class,'test1']);
+Route::get('cars', [CarController::class, 'index']);
+Route::get('/newws', [NewwsController::class, 'index'])->name('newws.index');
+Route::get('/newws/create', [NewwsController::class, 'create'])->name('newws.create');
+Route::post('/newws', [NewwsController::class, 'store'])->name('newws.store');
+Route::get('/newws/{newws}/edit', [NewwsController::class, 'edit'])->name('newws.edit');
+Route::put('/newws/{newws}/update', [NewwsController::class, 'update'])->name('newws.update');
+Route::delete('/newws/{newws}/destroy', [NewwsController::class, 'destroy'])->name('newws.destroy');
